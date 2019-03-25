7e8eae17089316a5f0ad6dd69d885c3d5d6a0959
mtkhawaja <36654508+mtkhawaja@users.noreply.github.com>
2019-03-24 21:43:48 -0400

    -User Login Verification tested.

--- app/src/main/java/com/example/instagram_clone/ParseApplication.java
+++ app/src/main/java/com/example/instagram_clone/ParseApplication.java
@@ -1,34 +1,19 @@
 package com.example.instagram_clone;
-
 import android.app.Application;
-
 import com.parse.Parse;
-
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 
 public class ParseApplication extends Application {
     private static final String appId     = "mtkhawaja-instagram-codepath";
     private static final String clientKey = "myMasterKeyIsASecretThatNoOneWillEverKnow";
-    private static final String serverURL = "http://mtkhawaja-instagram-codepath.herokuapp.com/parse/";
+    private static final String serverURL = "http://mtkhawaja-instagram-codepath.herokuapp.com/parse";
     @Override
     public void onCreate() {
         super.onCreate();
 
-        // Use for troubleshooting -- remove this line for production
-        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
-
-        // Use for monitoring Parse OkHttp traffic
-        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
-        // See http://square.github.io/okhttp/3.x/logging-interceptor/ to see the options.
-        OkHttpClient.Builder builder = new OkHttpClient.Builder();
-        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
-        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        builder.networkInterceptors().add(httpLoggingInterceptor);
-
         // set applicationId, and server server based on the values in the Heroku settings.
         // clientKey is not needed unless explicitly configured
         // any network interceptors must be added with the Configuration Builder given this syntax
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(appId) // should correspond to APP_ID env variable
                 .clientKey(clientKey)  // set explicitly unless clientKey is explicitly configured on Parse server
